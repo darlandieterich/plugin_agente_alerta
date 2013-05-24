0b390417277a9991f6407a421657e75cb6670467
Darlan dieterich <darlandieterich@gmail.com>
2013-05-23 23:04:44 -0300

    1.2.0
    Criação do desempenho de tarefas do usuário!

--- agente_alerta/block_agente_alerta.php
+++ agente_alerta/block_agente_alerta.php
@@ -26,7 +26,8 @@ class block_agente_alerta extends block_base
         $rlogin = array("Que tal fazer login?",
             "Olá vamos fazer o login?",
             "Que tal trabalhar um pouco? faça o login!",
-            "Vamos estudar? faça o login!");
+            "Vamos estudar? faça o login!",
+            "Confira suas tarefas! faça o login!");
 		//inicializa a variavel texto
         $this->content->text = "";
         //se usuario não estiver logado
@@ -41,7 +42,7 @@ class block_agente_alerta extends block_base
             $cursos = enrol_get_my_courses();
             //verifica se usuario esta matriculado
             if(count($cursos)==0)
-                $this->content->text .= "<b>".$USER->firstname."</b> você não esta matriculado em nenhum curso!";
+                $this->content->text .= "<strong>".$USER->firstname."</strong> você não esta matriculado em nenhum curso!";
             else
             {
                 //inicializa variavel como vazia
@@ -49,6 +50,7 @@ class block_agente_alerta extends block_base
                 ".count($cursos)." curso(s) <hr>";
                 $contaref = 0;
                 $cont_t_s = 0;
+                $totaltarefas = 0;
                 foreach ($cursos as $curso) {
                     
                     //sql para buscar tarefas que forem maiores de um periodo X
@@ -58,7 +60,7 @@ class block_agente_alerta extends block_base
                     //obtem resultado da consulta em forma de objeto
                     $rstarefas=$DB->get_records_sql($sql_tarefas,$parametro);
                     //imprime as avaliaçãoes ativas
-                    $this->content->text .= "<b><a href='course/view.php?id=".$curso->id."'>".$curso->fullname.":</a></b></br>";
+                    $this->content->text .= "<strong><a href='course/view.php?id=".$curso->id."'>".$curso->fullname.":</a></strong></br>";
                     //conta as tarefas
                     $totrs = count($rstarefas);
                     ////////////////////////////////////////
@@ -96,31 +98,65 @@ class block_agente_alerta extends block_base
                                 <a href='mod/assignment/view.php?id=".$this->get_id_assignment($dados->id)."'><img alt='Acessar tarefa' src='".$CFG->wwwroot.'/blocks/agente_alerta/pix/view_assign.gif'."'></a>]
                                 <span style='font-size:8px'> até ". date(" d/m/Y",$dados->timedue)."
                                 </span>
-                            </div>";                            
+                            </div>";
                         }
                     }else
                         $this->content->text .= "Sem Tarefas! <img alt='Sem tarefas!' src='".$CFG->wwwroot.'/blocks/agente_alerta/pix/smile.gif'."'></br>";
                     //acomula as tarefas//
                     $contaref += $totrs;
-                }                                
+                }
                 $this->content->text .= "<hr>";
-                $this->content->text .= "Tarefas em aberto:<b> ".$contaref."</b></br>";
-                $this->content->text .= "Tarefas submetidas:<b> ".$cont_t_s."</b></br>";
-                $this->content->text .= "Seu desempenho:<b> --</b>";                
-            }            
+                $this->content->text .= "Tarefas em aberto:<strong> ".$contaref."</strong></br>";
+                $this->content->text .= "Tarefas submetidas:<strong> ".$cont_t_s."</strong></br>";
+                /*
+                Calculo de desempenho, usando "Regra de Três" onde  
+                suas tarefas existentes por suas tarefas submetidas
+                FORMULA: ______100_______ * tarefasSubmetidas = VALOR
+                        tarefasExistentes
+                */
+                $this->content->text .= "Desempenho tarefas:<strong>".round((100/$this->count_assignmnet_user())*$this->count_assignment_submited())."%</strong>";
+            }
             //var_dump($USER);
             //echo(MOODLE_INTERNAL);
-            //$oi= assignment_count_real_submissions(CONTEXT_MODULE,null);            
+            //$oi= assignment_count_real_submissions(CONTEXT_MODULE,null);
             // retorna o conteudo do objeto content
             return $this->content;
-        }
-        //print_object($USER);        
+        }        
     }
-    function get_id_assignment($id){
+    function get_id_assignment($id)
+    {
         global $DB;
         $sql   = "SELECT * FROM mdl_course_modules cm WHERE cm.module = 1 AND cm.instance = ?";
         $param = array($id);
-        $data  = $DB->get_record_sql($sql,$param);        
-        return $data->id;        
-    }    
+        $data  = $DB->get_record_sql($sql,$param);
+        return $data->id;
+    }
+    function count_assignmnet_user()
+    {
+        global $DB;
+        $cursos = enrol_get_my_courses();
+        $cont_tarefas = 0;
+        foreach ($cursos as $curso)
+        {            
+            $sql_tarefas = "SELECT * FROM mdl_assignment asg WHERE asg.course = ?";
+            $parametro = array($curso->id);
+            $rstarefas=$DB->get_records_sql($sql_tarefas,$parametro);
+            foreach ($rstarefas as $tarefa) {
+                $cont_tarefas++;
+            }
+        }
+        return $cont_tarefas;
+    }
+    function count_assignment_submited()
+    {
+        global $DB,$USER;        
+        $cont_tarefas_sub = 0;                           
+        $sql_tarefas = "SELECT * FROM  mdl_assignment_submissions asb WHERE asb.userid = ?";
+        $parametro = array($USER->id);
+        $rstarefas=$DB->get_records_sql($sql_tarefas,$parametro);
+        foreach ($rstarefas as $tarefa) {
+            $cont_tarefas_sub++;
+        }        
+        return $cont_tarefas_sub;
+    }
 }
